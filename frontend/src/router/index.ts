8f6c5da89f92614e77b744d39de3556e59766b68
Carlos Eduardo Arismendi Sanchez <carlos.arismendisanchez@gmail.com>
2023-01-16 14:30:47 +0100

    Added html and styles of ChooseNicknameView

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import GameView from '../views/GameView.vue'
+import ChooseNickname from '../views/ChooseNicknameView.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
-            name: 'home',
+            name: 'choose-nickname',
+            component: ChooseNickname
+        },
+        {
+            path: '/game',
+            name: 'game',
             component: GameView
         }
     ]
---
+++ frontend/src/views/ChooseNicknameView.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+let playerID = ''
+function confirmNickname() {
+    console.log('confirmNickname: ', playerID)
+}
+</script>
+
+<template>
+    <main>
+        <form class="container h-auto position-absolute top-40 start-50 translate-middle">
+            <h1 class="row text-light d-flex justify-content-center mb-3">
+                <span class="col-12">Welcome to GoChess</span>
+            </h1>
+            <div class="row d-flex justify-content-center">
+                <div class="col-sm-12 col-lg-6 rounded border border-green py-2">
+                    <div class="mb-3">
+                        <label for="input-nickname" class="form-label text-light">Choose your nickname</label>
+                        <input
+                            type="text"
+                            class="form-control m-auto"
+                            id="input-nickname"
+                            placeholder="DarkGreatMaster"
+                            v-model="playerID"
+                        />
+                    </div>
+                    <button type="button" class="btn bg-green w-100" @click="confirmNickname">Confirm</button>
+                </div>
+            </div>
+        </form>
+    </main>
+</template>
+
+<style scoped>
+.top-40 {
+    top: 40% !important;
+}
+</style>
