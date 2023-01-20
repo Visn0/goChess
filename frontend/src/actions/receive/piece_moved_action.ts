80aa5fce5cd5eeab24dec3f93ca49295c8b9671b
Carlos Eduardo Arismendi Sanchez <carlos.arismendisanchez@gmail.com>
2023-01-20 09:15:27 +0100

    Added passant pawn in frontend

--- frontend/src/actions/receive/piece_moved_action.ts
+++ frontend/src/actions/receive/piece_moved_action.ts
@@ -1,4 +1,4 @@
-import type { File, PieceType, Rank } from '@/models/constants'
+import { PieceType, type File, type Rank } from '@/models/constants'
 import type { Game } from '@/models/game'
 import { Piece } from '@/models/piece'
 import type { Square } from '@/models/square'
@@ -27,11 +27,41 @@ class PieceMovedAction {
 
         const src: Square = this.game.board.getSquare(p.src.file, p.src.rank)
         const srcPiece = src.piece as Piece
+        const dst = this.game.board.getSquare(p.dst.file, p.dst.rank)
+
+        if (this.isPawnPassant(src, dst)) {
+            this.movePassant(src, dst)
+            return
+        }
 
         const dstPieceType = p.promoteTo ? new Piece(srcPiece.color, p.promoteTo) : srcPiece
 
         this.game.board.setSquarePiece(p.dst.file, p.dst.rank, dstPieceType)
         this.game.board.setSquarePiece(src.file, src.rank, null)
+        this.game.unselectSrcSquare()
+    }
+
+    private isPawnPassant(src: Square, dst: Square): boolean {
+        if (src.piece?.type !== PieceType.PAWN) {
+            return false
+        }
+
+        // Passant must be diagonal (different file)
+        if (src.file === dst.file) {
+            return false
+        }
+
+        return dst.isEmpty()
+    }
+
+    private movePassant(src: Square, dst: Square) {
+        // Remove captured pawn
+        this.game.board.setSquarePiece(dst.file, src.rank, null)
+
+        // Move killer pawn
+        this.game.board.setSquarePiece(dst.file, dst.rank, src.piece)
+        this.game.board.setSquarePiece(src.file, src.rank, null)
+
         this.game.unselectSrcSquare()
     }
 }
