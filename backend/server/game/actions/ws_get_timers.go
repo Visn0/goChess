c8aa1fba241e7bd02f80b6a635e312d254e600eb
Carlos Eduardo Arismendi Sanchez <carlos.arismendisanchez@gmail.com>
2023-01-21 17:37:37 +0100

    Added sync of timers between players in frontend
    Now frontend request both timers every second. Maybe an improvements could be that frontend calculates the timers on it own and only request them every N seconds to sync

--- backend/server/game/actions/ws_get_timers.go
+++ backend/server/game/actions/ws_get_timers.go
@@ -1,11 +1,14 @@
 package actions
 
-import "chess/server/shared"
+import (
+	"chess/server/shared"
+	"log"
+)
 
 type ResponseTimers struct {
 	Action     string `json:"action"`
-	PlayerTime int    `json:"player-time"`
-	EnemyTime  int    `json:"enemy-time"`
+	PlayerTime int    `json:"playerTime"`
+	EnemyTime  int    `json:"enemyTime"`
 }
 
 func WsGetTimers(p1, p2 *shared.WsConn, t1, t2 int) {
@@ -19,4 +22,6 @@ func WsGetTimers(p1, p2 *shared.WsConn, t1, t2 int) {
 		PlayerTime: t2,
 		EnemyTime:  t1,
 	})
+
+	log.Println("=>> timers =>> p1: ", t1/1000, "| p2: ", t2)
 }
